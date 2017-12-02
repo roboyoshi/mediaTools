67466166f28d3da6a97fb25e052d027b2e989200
roboyoshi <robdidop@protonmail.com>
2017-12-02 20:57:33 +0100

    2nd branch init: Import songs with pyObjC (Scripting Bridge)
    - trying to find out if this is faster

--- iTunesWatchFolder.py
+++ iTunesWatchFolder.py
@@ -1,9 +1,8 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python3
 
 """
 	iTunesWatchFolder Script
 	- Synchronize iTunes Library <--> Folder
-	
 	Written by u/RoboYoshi
 """
 
@@ -11,25 +10,30 @@ import sys, time, itertools, threading, queue
 import os, subprocess, re, plistlib, datetime, shutil
 from urllib.parse import quote, unquote, urlparse
 from pprint import pprint as pp
+from Foundation import NSURL
+from ScriptingBridge import SBApplication
 
-debug = False
-backup = True
-addnew = True
-rmdead = True
+DEBUG = False
+CREATE_BACKUP = True
+ADD_NEW_TRACKS = True
+REMOVE_DEAD_TRACKS = False
 
 # If used in Application Context
-if(debug): print("[Debug]\t sys.argv = ", sys.argv)
-if(len(sys.argv))>1:
-	musicFolder = sys.argv[1]
+if DEBUG: print("[Debug]\t sys.argv = ", sys.argv)
+if len(sys.argv) > 1:
+    musicFolder = sys.argv[1]
 else:
 	print("[Warn]\t No Library given to sync with.")
-	sys.exit(1)
+	musicFolder = "/Volumes/audio/Library/podcasts"
+	# sys.exit(1)
 
 allowedExtensions=('mp3', 'm4a', 'm4b') # can be extended to your liking
 
 def loadingAnimation():
 	for c in itertools.cycle(['|', '/', '-', '\\']):
 		if done:
+			sys.stdout.write('\r')
+			sys.stdout.flush()
 			break
 		sys.stdout.write('\r' + c)
 		sys.stdout.flush()
@@ -45,7 +49,6 @@ def mkdir_p(path):
 		else:
 			raise
 
-
 def getiTunesXMLPath():
 	"""
 	Read iTunes XML Path from user defaults
@@ -59,14 +62,14 @@ def getiTunesXMLPath():
 	if(xmlMatch):
 		xmlPath = xmlMatch.group(1).replace("file://","")
 		xmlPath = unquote(xmlPath)
-		if(debug): print("[Debug]\t xmlPath = %s" % xmlPath)
+		if(DEBUG): print("[Debug]\t xmlPath = %s" % xmlPath)
 		return xmlPath
 	else:
 		print("[Error]\t Could not get XML Path from defaults. Are you sharing your XML in iTunes -> Settings -> Advanced?")
 		return 1
 
 def backupLibraryDB(xmlPath):
-	print("[Info]\t Create Backup of iTunes Library DB.")
+	print("[Info]\t Creating Backup of iTunes Library DB.")
 	backupFiles = ['iTunes Library Extras.itdb', 'iTunes Library Genius.itdb', 'iTunes Library.itl', 'iTunes Library.xml', 'iTunes Music Library.xml']
 	libPath = os.path.dirname(xmlPath)
 	timestamp = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
@@ -78,44 +81,72 @@ def backupLibraryDB(xmlPath):
 		try:
 			shutil.copy(libFilePath, bakFilePath)
 		except:
-			if(debug): print("[Debug]\t libFile Missing: %s" % libFile)
+			if(DEBUG): print("[Debug]\t libFile Missing: %s" % libFile)
 			pass
-	if(debug): print("[Debug]\t Created a Backup of your iTunes Library in %s" % backupFolder)
+	if(DEBUG): print("[Debug]\t Created a Backup of your iTunes Library in %s" % backupFolder)
+	return 0
 
-
-def getTracksFromiTunesXML(xmlPath):
+def getTracksFromiTunesXML(xmlPath, noNSURL=False):
 	"""
 	Extract Track Paths from iTunes XML
-	:param xmlPath: Path to your iTunes Library XML (Plist) file
-	:return list: List with all Tracks
+	:param xmlPath:
+		Path to your iTunes Library XML (Plist) file
+	:param noNSURL:
+		If False, the track Path is returned
+	:return list:
+		List with all Tracks
 	"""
 	# Thanks to https://github.com/liamks/libpytunes for some pointers
 	print("[Info]\t Fetching all Tracks from iTunes Library..")
-	libTracks = [] # RAW POSIX PATH PLS
+	libTracks = []
 	library = plistlib.readPlist(xmlPath)
 	for trackid, attributes in library['Tracks'].items():
 		if attributes.get('Location'):
-			location = unquote(urlparse(attributes.get('Location')).path)
-			if(debug): print("[Debug]\t Track Path in iTunes = %s" % location)
+			trackPath = attributes.get('Location')
+			if(DEBUG): print("[Debug]\t Track Path in iTunes = %s" % trackPath)
+			# By default we take the raw NSURL from the iTunes.xml
+			trackPath = unquote(urlparse(trackPath).path)
+			if(noNSURL):
+				location = trackPath
+			else:
+				# NoTE: We shall transform the locationAttr of the track into an NSURL Object
+				# While we sure CAN, it's not useful:
+				# the tracks would be double-encoded and gain other hashes for comparision later
+				location = NSURL.fileURLWithPath_(trackPath)
 			libTracks.append(location)
 	print("[Info]\t Tracks in iTunes = %i" % len(libTracks))
-	q_libTracks.put(libTracks)
+	libTracksQueue.put(libTracks) # Put result in Queue
 	return libTracks
 
-def getTracksFromFolder(dirPath, ext=('mp3', 'm4a')):
+def getTracksFromFolder(dirPath, ext=('mp3', 'm4a'), noNSURL=False):
+	"""
+	Retrieve all Songs from Folder
+	:param dirPath:
+		filePath to Music as String
+	:param ext:
+		A tuple list of allowed extensions
+	:param noNSURL:
+		Set True/False if you want to return Songs as NSURL Paths
+		Note that the Songs need to be conform with iTunes Extractions for comparision.
+	"""
 	print("[Info]\t Search for Files in Folder with allowed Extensions..")
-	dirTracks = [] # RAW POSIX PATH PLS
+	dirTracks = []
 	count = 0
 	for root, dirs, files in os.walk(dirPath):
 		for name in files:
 			if(name.lower().endswith(ext)):
 				track = os.path.join(root, name)
-				dirTracks.append(track)
+				if(noNSURL):
+					location = track
+				else:
+					location = NSURL.fileURLWithPath_(track)
+				dirTracks.append(location)
+				if(DEBUG): print("[Debug]\t Track Path in Folder = %s" % location)
 				count+=1
 				if count % 500 == 0:
-					if(debug): print("[Debug]: Found %s Tracks.." % count)
+					if(DEBUG): print("[Debug]: Found %s Tracks.." % count)
 	print("[Info]\t Tracks in Folder = %i" % len(dirTracks))
-	q_dirTracks.put(dirTracks)
+	dirTracksQueue.put(dirTracks)
 	return dirTracks
 
 def filterTracksForImport(dirTracks, libTracks):
@@ -125,32 +156,28 @@ def filterTracksForImport(dirTracks, libTracks):
 	:return list: sorted list with all missing tracks
 	"""
 	newTracks = list(set(dirTracks) - set(libTracks))
-	if(debug): print("[Debug]\t New Tracks in Folder = %i" % len(newTracks))
-	return sorted(newTracks)
+	# newTracks = sorted(newTracks) # NSURL are not sortable?
+	print("[Info]\t New Tracks to Import = %i" % len(newTracks))		
+	return newTracks
 
-def importTracksToiTunes(newTracks):
+def importTracksToiTunesViaSB(newTracks):
 	print("[Info]\t Adding new Tracks to iTunes.")
 	"""
 	Import a Bunch of Tracks into iTunes
 	:param newTracks: Python List with all new Tracks
 	:return int: 0
 	"""
+	iTunes = SBApplication.applicationWithBundleIdentifier_("com.apple.iTunes")
+	count = 0
+	batchSize = 100
+	batch = []
 	for track in newTracks:
-		if(debug): print("[Debug]\t TrackPath = %s" % track)
-		# Script Notes: Unicode is needed for files with special characters.
-		script = '''
-        on run {input}
-			set trackFile to POSIX file input as Unicode text
-			tell app "iTunes"
-				add trackFile to playlist 1
-			end tell
-        end run
-		'''
-		args = [track]
-		p = subprocess.Popen(['/usr/bin/osascript', '-'] + args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-		stdout, stderr = p.communicate(script.encode('UTF-8'))
-		if(debug):
-			if(stderr!= b''): print("[Debug]\t AppleScript Error: %s" % stderr)
+		if(DEBUG): print("[Debug]\t TrackPath = %s" % track)
+		batch.append(track)
+		count += 1
+		if(batchSize % count == 0):
+			iTunes.add_to_(batch, None)
+			batch.clear()
 	return 0
 
 def removeDeadTracksFromiTunes():
@@ -165,7 +192,7 @@ def removeDeadTracksFromiTunes():
 	'''
 	p = subprocess.Popen(['/usr/bin/osascript', '-'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	stdout, stderr = p.communicate(script.encode('UTF-8'))
-	if(debug):
+	if(DEBUG):
 		if(stderr!= b''): print("[Debug]\t AppleScript Error: %s" % stderr)
 	return 0
 
@@ -174,47 +201,47 @@ def removeDeadTracksFromiTunes():
 xmlPath = getiTunesXMLPath()
 
 # Backup iTunes Library Files
-if(backup): backupLibraryDB(xmlPath)
+if(CREATE_BACKUP): backupLibraryDB(xmlPath)
 
 # Prepare Queues to store values
-q_libTracks = queue.Queue()
-q_dirTracks = queue.Queue()
+libTracksQueue = queue.Queue()
+dirTracksQueue = queue.Queue()
 
 # Declare Threads
-t_libTracks = threading.Thread(target=getTracksFromiTunesXML, args=(xmlPath,))
-t_dirTracks = threading.Thread(target=getTracksFromFolder, args=(musicFolder,), kwargs={'ext' : allowedExtensions})
+libTracksThread = threading.Thread(target=getTracksFromiTunesXML, args=(xmlPath,))
+dirTracksThread = threading.Thread(target=getTracksFromFolder, args=(musicFolder,), kwargs={'ext' : allowedExtensions})
 
 done = False
-t_loading = threading.Thread(target=loadingAnimation)
-t_loading.start()
+loadingThread = threading.Thread(target=loadingAnimation)
+loadingThread.start()
 
 # Start both Threads
-t_libTracks.start()
-t_dirTracks.start()
+libTracksThread.start()
+dirTracksThread.start()
 
 # Wait until both are finished
-t_libTracks.join()
-t_dirTracks.join()
+libTracksThread.join()
+dirTracksThread.join()
 
 done = True
 # Get Arrays from Thread-Queues
-libTracks = q_libTracks.get()
-dirTracks = q_dirTracks.get()
+libTracks = libTracksQueue.get()
+dirTracks = dirTracksQueue.get()
 
 # Diff Arrays and only keep Tracks not already in Library
 newTracks = filterTracksForImport(dirTracks, libTracks)
 
 if(len(newTracks) != 0):
 	# Import Tracks into iTunes with AppleScript
-	if(addnew):
+	if(ADD_NEW_TRACKS):
 		done = False
-		t_loading = threading.Thread(target=loadingAnimation)
-		t_loading.start()
-		importTracksToiTunes(newTracks)
+		loadingThread = threading.Thread(target=loadingAnimation)
+		loadingThread.start()
+		importTracksToiTunesViaSB(newTracks)
 		done = True
 else: print("[Info]\t No New Tracks.")
 
 # Remove Dead Tracks from iTunes
-if(rmdead): removeDeadTracksFromiTunes()
+if(REMOVE_DEAD_TRACKS): removeDeadTracksFromiTunes()
 
 # EOF
